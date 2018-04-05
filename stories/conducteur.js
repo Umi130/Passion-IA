8bf57c627bc23ca1ffc78874c929054298816fb7
LBD <audio@PST8532.sciences-po.fr>
2018-04-05 19:20:28 +0200

    MàJ conducteur

--- stories/conducteur.js
+++ stories/conducteur.js
@@ -108,7 +108,8 @@ const conducteur = [
 	},
 
 //////////////
-// MINI-JEU RECONNAISSANCE passage piéton
+// MINI-JEU 1/4
+// RECONNAISSANCE passage piéton
 // YES-NO
 //////////////
 
@@ -156,16 +157,16 @@ const conducteur = [
 		title: "Est-ce un passage piéton ? (11/11)",
 		image: "robot-conducteur-reconnaitre-passage-11.jpg",
 	},	
-
 //////////////
-// MINI-JEU RECONNAISSANCE passage piéton
+// FIN MINI-JEU 1/4
+// RECONNAISSANCE passage piéton
 // YES-NO
 //////////////
 
 	{
 		title: "Bravo !",
-		message: "Vous avez passé la première étape du labelling, vous félicite Erik Coelingh.",
-		image: "experts-conducteur-coelingh.jpg",
+		message: "Vous avez passé la première phase du «labelling».",
+		image: "robot-conducteur-bebe.png",
 	},
 
 ////////////////
@@ -181,97 +182,248 @@ const conducteur = [
 //
 
 	{
-		message: "Maintenant que votre voiture sait reconnaître les passages piétons, vos ingénieurs vont s’occuper de lui apprendre à reconnaître les voitures, les camions, etc.",
+		message: "Maintenant que votre voiture sait reconnaître les passages piétons, vos ingénieurs vont s’occuper de lui apprendre à reconnaître les voitures, les camions, et ainsi de suite.",
+		image: "robot-conducteur-enfant.png",
+	},
+	{
+		title: "Vous pouvez procéder à la deuxième phase : la localisation dans l'espace",
+		message: "Il ne suffit pas que {{name}} puisse reconnaître les objets, la voiture doit aussi pouvoir les placer dans l’espace.",
 		image: "robot-conducteur-enfant.png",
 	},
 
-
+//////////////
+// MINI-JEU 2/4
+// RECONNAISSANCE route présence piétons
+// YES-NO
+//////////////
 	{
-		message: "Après tous ces tests, votre voiture a enfin obtenu les autorisations pour des essais sur la route. Vous sortez votre véhicule pour le faire rouler en ville.",
-		image: "robot-conducteur-adulte.png",
+		title: "Des personnes sont-ils sur la route ? (1/7)",
+		image: "robot-conducteur-reconnaitre-route-1.jpg",
 	},
 	{
-		message: "Tout se passe bien, lorsqu’un qu’un scooter vous double par la droite, alors que vous vous apprêtiez à prendre un virage à droite. Vous le percutez et lui cassez une jambe à cause du choc. ",
-		image: "robot-conducteur-adulte.png",
+		title: "Des personnes sont-ils sur la route ? (2/7)",
+		image: "robot-conducteur-reconnaitre-route-2.jpg",
 	},
-
-/// Choix responsabilité à compléter Pas de bonne ou mauvaise réponse ///
+	{
+		title: "Des personnes sont-ils sur la route ? (3/7)",
+		image: "robot-conducteur-reconnaitre-route-3.jpg",
+	},
+	{
+		title: "Des personnes sont-ils sur la route ? (4/7)",
+		image: "robot-conducteur-reconnaitre-route-4.jpg",
+	},
+	{
+		title: "Des personnes sont-ils sur la route ? (5/7)",
+		image: "robot-conducteur-reconnaitre-route-5.jpg",
+	},
+	{
+		title: "Des personnes sont-ils sur la route ? (6/7)",
+		image: "robot-conducteur-reconnaitre-route-6.jpg",
+	},
+	{
+		title: "Des personnes sont-ils sur la route ? (7/7)",
+		image: "robot-conducteur-reconnaitre-route-7.jpg",
+	},
+//////////////
+// FIN MINI-JEU 2/4
+// RECONNAISSANCE route présence piétons
+// YES-NO
+//////////////
 
 	{
-		name: "choice_responsabilite",
-		message: "Que faites-vous ?",
+		title: "Bravo !",
+		message: "Vous avez passé la seconde phase phase du «labelling». {{name}} sait désormais reconnaître les objets et peut les placer sur une image.",
+		image: "robot-conducteur-enfant.png",
+	},
+
+//
+// QUESTION BASE DE DONNEES
+//
+	{
+		name: "choice_basedonnees",
+		title: "Base de données",
+		message: "Il est maintenant temps de fournir à {{name}} des données à analyser. Que choisissez-vous ?",
 		choices: [
 			{
-				"value": "refus de responsabilité",
-				"description": "Vous et Vroumvroum refusez d’endosser la responsabilité de cet accident.",
+				"value": "Le code de la route",
+				"points": 0
+			},
+			{
+				"value": "Les lois de la physique",
+				"points": 1
+			},
+		]
+	},
+
+//Si bonne réponse
+	{
+		title: "Vous faites bien !",
+		message: "Il faut avant tout que la voiture apprenne les lois de la physique élémentaire, surtout en ce qui concerne la vitesse. L’apprentissage du code de la route viendra après.",
+		image: "robot-conducteur-enfant.png",
+		when: answers => answers.choice_basedonnees !== 'Le code de la route',
+	},
+//Si mauvaise réponse
+	{
+		title: "Attention !",
+		message: "il faut apprendre le code de la route, mais cette étape vient bien après. La voiture doit avant tout connaître les lois de la physique élémentaire, surtout en ce qui concerne la vitesse.",
+		image: "robot-conducteur-enfant-etonne.png",
+		when: answers => answers.choice_basedonnees === 'Le code de la route',
+	},
+
+	{
+		title: "Votre voiture comprend maintenant la physique de base",
+		message: "Par exemple, si elle va à 100 km/h sur une route sèche, il lui faudra 80 mètres pour s’arrêter complètement.",
+		image: "robot-conducteur-enfant.png",
+	},
+	{
+		title: "La physique est importante pour l'intelligence artificielle des voitures",
+		message: "Sa compréhension va permettre d’anticiper la trajectoire des véhicules alentours.",
+		image: "robot-conducteur-enfant.png",
+	},
+	{
+		message: "Ainsi, si le camion derrière vous va trop vite pour pouvoir s’arrêter en cas de freinage d’urgence, {{name}} pourra choisir de se déporter.",
+		image: "robot-conducteur-enfant.png",
+	},
+
+////////////////
+// ROBOT ADO
+////////////////
+
+// Robot devient ado
+	{
+		title: "Quoi ?",
+		message: "{{name}} a grandi ! Elle va bientôt pouvoir rouler par elle-même, mais il lui manque encore quelques données.",
+		image: "robot-conducteur-ado.png",
+	},
+//
+
+	{
+		title: "Apprentissage du code de la route",
+		message: "Il faut aider {{name}} à reconnaître les panneaux de signalisation routière.",
+		image: "robot-conducteur-ado.png",
+	},
+
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+
+////////////////
+// ROBOT ADULTE
+////////////////
+
+// Robot devient adulte
+	{
+		title: "Quoi ?",
+		message: "{{name}} a grandi !",
+		image: "robot-conducteur-adulte.png",
+	},
+//
+
+	{
+		title: "Après tous ces tests...",
+		message: "{{name}} a enfin obtenu les autorisations pour des essais sur la route ! Vous sortez votre voiture pour la faire rouler en ville.",
+		image: "robot-conducteur-adulte.png",
+	},
+	{
+		title: "Tout se passe bien, mais soudain...",
+		message: "Un scooter vous double par la droite, alors que vous vous apprêtiez à prendre un virage à droite. Vous le percutez et lui cassez une jambe à cause du choc.",
+		image: "robot-conducteur-adulte-etonne.png",
+	},
+
+/// Choix responsabilité à compléter ///
+/// Pas de bonne ou mauvaise réponse ///
+
+//
+// QUESTION RESPONSABILITE
+//
+	{
+		name: "choice_responsabilite",
+		title: "Que choisissez-vous ?",
+		choices: [
+			{
+				"value": "Refus de responsabilité",
+				"description": "Vous et Vroumvroum refusez d’endosser la responsabilité de cet accident",
 				"points": 1
 			},
 			{
-				"value": "prise de responsabilité",
-				"description": "Vous et Vroumvoum vous portez responsable.",
+				"value": "Prise de responsabilité",
+				"description": "Vous et Vroumvoum vous portez responsable",
 				"points": 1
 			},
 		]
 	},
 	{
 		title: "Une enquête est ouverte",
-		message: "C’est le premier incident impliquant une voiture autonome en France, les autorités prennent donc ce cas au sérieux.",
-		image: "robot-conducteur-adulte.png",
+		message: "C’est le premier incident impliquant une voiture autonome en France. Les autorités prennent donc ce cas au sérieux.",
+		image: "robot-conducteur-adulte-etonne.png",
 	},
 	{
 		title: "La zone grise de la législation",
-		message: "Il n’y a pas encore de texte de loi encadrant la responsabilité des voitures autonomes en France, votre accident va donc faire jurisprudence. ",
-		image: "robot-conducteur-adulte.png",
+		message: "Il n’y a pas encore de texte de loi encadrant la responsabilité des voitures autonomes en France. Votre accident va donc faire jurisprudence.",
+		image: "robot-conducteur-adulte-etonne.png",
 	},
 
-/// Parcours refus de responsabilité ///
+//
+// QUESTION RESPONSABILITE
+//
+/// Parcours refus de responsabilité
 	
 	{
-		message: "Mieux vaut vous entourer d’avocats pour vous protéger si le conducteur du scooter décide de porter plainte.",
-		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "refus de responsabilité",
+		message: "Mieux vaut vous entourer d’avocats pour vous protéger, si le conducteur du scooter décide de porter plainte.",
+		image: "robot-conducteur-adulte-etonne.png",
+		when: answers => answers.choice_responsabilite === "Refus de responsabilité",
 	},
 	{
-		message: "Dans votre cas, {{name}} n’était pas en faute : c’est le scooter qui n’a pas respecté le code de la route.",
-		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "refus de responsabilité",
+		title: "Dans votre cas...",
+		message: "{{name}} n’était pas en faute : c’est le scooter qui n’a pas respecté le code de la route.",
+		image: "robot-conducteur-adulte-etonne.png",
+		when: answers => answers.choice_responsabilite === "Refus de responsabilité",
 	},
 	{
-		message: "Impossible par contre de savoir si un conducteur aurait pu éviter le scooter. L’affaire se termine par un non-lieu.",
-		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "refus de responsabilité",
+		message: "En revanche, impossible de savoir si un conducteur aurait pu éviter le scooter. L’affaire se termine par un non-lieu.",
+		image: "robot-conducteur-adulte-etonne.png",
+		when: answers => answers.choice_responsabilite === "Refus de responsabilité",
 	},
 	{
 		title: "Un coup dur pour l'industrie du véhicule autonome...",
-		message: "Les constructeurs ont mis en pause leurs tests et l’opinion du public sur les voitures automatiques est maintenant négative...",
-		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "refus de responsabilité",
+		message: "Les constructeurs ont mis en pause leurs tests et l’opinion du public sur les voitures automatiques est maintenant négative.",
+		image: "robot-conducteur-adulte-etonne.png",
+		when: answers => answers.choice_responsabilite === "Refus de responsabilité",
 	},
 
-/// Parcours prise de responsabilité ///
+//
+// QUESTION RESPONSABILITE
+//
+/// Parcours prise de responsabilité
 
 	{
 		message: "Puisque vous avez décidé de prendre vos responsabilités, vous allez avoir besoin d’avocats pour vous défendre.",
 		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "prise de responsabilité",
+		when: answers => answers.choice_responsabilite !== "Refus de responsabilité",
 	},
 	{
+		message: "Dans votre cas...",
 		message: "{{name}} n’était pas en faute : c’est le scooter qui n’a pas respecté le code de la route. Mais le juge veut faire de ce cas un exemple, et comme vous vous portez responsable, vous êtes condamnés à payer une amende.",
 		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "prise de responsabilité",
+		when: answers => answers.choice_responsabilite !== "Refus de responsabilité",
 	},
 	{
 		title: "Un coup dur pour l'industrie du véhicule autonome...",
-		message: "Beaucoup de projets ont été interrompus. Cependant vous avez réussi à rassurer le public en acceptant d’endosser la responsabilité de l’accident. ",
+		message: "Beaucoup de projets ont été interrompus. Cependant, vous avez réussi à rassurer le public en acceptant d’endosser la responsabilité de l’accident.",
 		image: "robot-conducteur-adulte.png",
-		when: answers => answers.choice_responsabilite === "prise de responsabilité",
+		when: answers => answers.choice_responsabilite !== "Refus de responsabilité",
 	},
-
-/// Retour au parcours commun ///
+//
+// QUESTION RESPONSABILITE
+// Fin de la question
 
 	{
-		message: "Un cas similaire est arrivé aux Etats-Unis. Une voiture Uber en phase de test a renversé une piétonne, qui est décédée. L’enquête n’a pas encore livré ses conclusions, mais a laissé toute l’industrie en suspens.",
-		image: "robot-conducteur-adulte.png",
+		title: "Un cas similaire s'est produit aux États-Unis",
+		message: "Une voiture Uber en phase de test a renversé une piétonne, qui est décédée. L’enquête n’a pas encore livré ses conclusions, mais a laissé toute l’industrie en suspens.",
+		image: "robot-conducteur-adulte-etonne.png",
 	},
 	{
 		message: "Depuis, les chercheurs se sont posés des questions sur la réaction à adopter en cas d’accident. Que se passe-t-il si la voiture doit faire un choix ? Il faut définir un code moral pour que le véhicule sache quoi faire.",
@@ -279,75 +431,77 @@ const conducteur = [
 	},
 
 	{
-		name: "choice_accident_inevitable",
+		name: "choice_accidentinevitable",
 		title: "Dans le cas d’un accident inévitable, qui choisissez-vous de sauver ?",
 		choices: [
 			{
-				"value": "Les passagers de la voiture",
-				"description": "",
+				"value": "Les passagers",
+				"description": "de la voiture",
 				"points": 1
 			},
 			{
-				"value": "Les autres usagers de la route",
-				"description": "",
-				"points": 1
-			},
-			{
-				"value": "Les utilisateurs n’ayant pas commis d’infraction au code de la route.",
-				"description": "",
+				"value": "Les piétons",
+				"description": " ",
 				"points": 1
 			}
 			]
 	},
+
 	{
-		message: "Vous avez eu du mal à choisir? C'est normal... Il n'y a pas de bonne réponse à cette question. Chaque situation a ses particularités et la décision ne dépend pas que des faits : elle fait appel à votre morale.",
+		title: "Vous avez eu du mal à faire un choix ?",
+		message: "C'est normal : il n'y a pas de bonne réponse à cette question. Chaque situation a ses particularités et la décision ne dépend pas que des faits : elle fait appel à votre morale.",
 		image: "robot-conducteur-adulte.png",
 	},
-/// Compléter avec l'image de capture écran de moral machine///	
+
 	{
 		message: "Le site Moral Machine propose à ses utilisateurs de prendre différentes décisions lorsqu'une situation d'accident se présente. Les statistiques récoltées fournissent un aperçu de la position de l’opinion publique sur ces questions.",
-		image: "",
+		image: "robot-conducteur-moralmachine.jpg",
 	},
 	{
-		message: "Ces résultats permettent de mettre en lumière les questions morales que les concepteurs de voitures autonomes doivent se poser. Mais avant de penser à tout cela, voyons si votre véhicule pourrait rouler, en fonction de vos réponses tout au long du parcours. ",
+		message: "Ces résultats permettent de mettre en lumière les questions morales que les concepteurs de voitures autonomes doivent se poser.",
+		image: "robot-conducteur-moralmachine.jpg",
+	},
+
+	{
+		message: "Mais avant de penser à tout cela, voyons si votre véhicule pourrait rouler, en fonction de vos réponses tout au long du parcours.",
 		image: "robot-conducteur-adulte.png",
 	},
+
 
 /// ETAPE CALCUL DES POINTS A AJOUTER /// 
 
+/// VICTOIRE
 /// Parcours véhicule opérationnel (26 points ou plus) WHEN à rajouter IMAGE a rajouter/// 
-
 	{
-		message: "Même si votre véhicule autonome est prometteur, le manque de législation vous empêche de le commercialiser.",
-		image: "",
+		title: "Votre véhicule autonome est prometteur !",
+		image: "robot-conducteur-adulte.png",
 	},
 	{
-		message: "Justement, le député Cédric Villani a proposé la création d’un comité éthique pour répondre à ces questions dans un rapport de mars 2018.",
-		image: "",
+		title: "Mais le manque de législation vous empêche de le commercialiser",
+		message: "Justement, le député Cédric Villani a proposé la création d’un comité éthique pour répondre à ces problématiques dans un rapport de mars 2018.",
+		image: "experts-juriste-villani.jpg",
 	},
 	{
 		message: "En attendant, cela vous laisse du temps pour continuer à améliorer {{name}}, notamment en vous penchant sur les enjeux moraux que soulève votre technologie.",
-		image: "",
+		image: "robot-conducteur-adulte.png",
 	},
 
+/// Échec
 /// Parcours véhicule non opérationnel (moins de 26 points) WHEN ET IMAGE FIN DU SCENARIO à ajouter ///
 
-
 	{
-		message: "L’accident avec le scooter vous l’a montré, votre voiture autonome n’est pas encore totalement prête.",
-		image: "",
+		title: "Votre véhicule autonome n'est vraiment pas prêt...",
+		message: "L’accident avec le scooter vous l’a montré, {{name}} n’est pas encore totalement opérationnelle.",
+		image: "robot-conducteur-adulte-etonne.png",
 	},
 	{
-		message: "En attendant qu’elle le soit, le député Cédric Villani a proposé dans un rapport de mars 2018 la création d’un comité éthique pour étudier les questions morales liés aux accidents de voitures autonomes.",
-		image: "",
+		message: "En attendant, le député Cédric Villani a proposé dans un rapport de mars 2018 la création d’un comité éthique pour étudier les questions morales liés aux accidents de voitures autonomes.",
+		image: "experts-juriste-villani.jpg",
 	},
 	{
-		message: "Pour l’instant, votre objectif de base - réduire le nombre de morts sur la route - n’est pas encore atteint, il va vous falloir travailler davantage sur votre voiture autonome.",
-		image: "",
+		message: "Pour l’instant, votre objectif de base - réduire le nombre de morts sur la route - n’est pas encore atteint. Il va vous falloir davantage travailler sur votre voiture autonome.",
+		image: "robot-conducteur-adulte-etonne.png",
 	},
-
-
-	//// Crédits à ajouter 
 
 ]
 
