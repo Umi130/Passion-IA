93dcb59759de7816b0e2c080b60247279f39f63e
MathildeDumazet <31658369+MathildeDumazet@users.noreply.github.com>
2018-06-20 12:07:36 +0200

    résultat mini jeu musique commentaire

--- stories/musicien.js
+++ stories/musicien.js
@@ -842,6 +842,20 @@ const musicien = [
 		when: answers => !answers.blanks_sons_2 && answers.choice_mode_composition === "Celle de Robin Meier",
 	},
 
+	//{	
+		// title: "C’est un succès !",
+		// message: "Avec un score de {{contextPoints.minijeu_instruments}} sur 3, vous avez pu vous constituer une bibliothèque de sons que vous allez pouvoir mélanger pour créer de nouveaux instruments.",
+		// image: "robot-musicien-bebe.png",
+		// when: answers => answers.choice_mode_composition == "Celle de Robin Meier" && answers.contextPoints.minijeu_instruments >= 2,
+	// },
+// Mauvaise réponse
+	// {	
+		// title: "C’est plutôt un échec...",
+		// message: "Avec un score de {{contextPoints.minijeu_instruments}} sur 3, il va vous falloir passer un peu plus de temps sur la labellisation des instruments pour que votre bibliothèque soit complète. Ce n'est qu'après que vous pourrez créer de nouveaux sons en mélangeant des enregistrements existants.",
+		// image: "robot-musicien-bebe-etonne.png",
+		// when: answers => answers.choice_mode_composition == "Celle de Robin Meier" && answers.contextPoints.minijeu_instruments < 2,
+	// },
+
 // COUPE POUR EGALISATION SCORE.
 // // Jeu 3 son
 // 	{
